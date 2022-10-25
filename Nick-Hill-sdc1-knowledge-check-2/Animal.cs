8e02bd2be463a7c9a15e38563ee4dfeb73ecd8a1
Nick Hill <nicholaspatrickhill@gmail.com>
2022-10-25 15:58:03 -0400

    initial commit

---
+++ Nick-Hill-sdc1-knowledge-check-2/Animal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nick_Hill_sdc1_knowledge_check_2
+{
+    abstract class Animal
+    {
+    }
+}
---
+++ Nick-Hill-sdc1-knowledge-check-2/Cat.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nick_Hill_sdc1_knowledge_check_2
+{
+    public class Cat : Animal
+    {
+    }
+}
